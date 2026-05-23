53f2e44f21e1d5ac60d7e4eaae72d4e757d4fd5b
Nicolás <amongbytes@gmail.com>
2026-05-22 21:48:13 -0300

    Agrego estructura inicial de tests para saucedemo

---
+++ tests/test_saucedemo.py
@@ -0,0 +1,11 @@
+def test_multiplicacion():
+    resultado = 3 * 5
+    assert resultado == 15
+
+def test_suma():
+    resultado = 10 + 5
+    assert resultado == 15
+
+def test_division():
+    resultado = 10 / 2
+    assert resultado == 5
